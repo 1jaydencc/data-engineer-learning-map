c3838a5cb5232d0150377a4f1ab5ad8f6e318b0b
1jaydencc <100813722+1jaydencc@users.noreply.github.com>
2023-09-05 18:30:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,26 +5,18 @@ def render_mermaid_chart(diagram_code):
     with open("mermaid_component/mermaid_component.html", "r") as f:
         html_code = f.read().replace("{{ diagram_code }}", diagram_code)
     components.html(html_code, height=800)
-
 st.title("Learning Path")
-
 learning_path = {
     "Beginner": ["Task 1", "Task 2", "Task 3"],
     "Intermediate": ["Task 4", "Task 5"],
     "Advanced": ["Task 6", "Task 7", "Task 8", "Task 9"],
 }
-
 diagram_code = "graph TD\n"
 prev_level = None
-
 for level, tasks in learning_path.items():
     for i, task in enumerate(tasks):
-        task_key = f"{level}_{task}"
-        checked = st.checkbox(task, value=task_states[task_key], key=task_key)
-        task_states[task_key] = checked
         checked = st.checkbox(task)
         node_id = f"{level[0]}{i}"
-
         diagram_code += f'{node_id}["{task} {"(Done)" if checked else ""}"]\n'
         if prev_level:
             diagram_code += f"{prev_level} --> {node_id}\n"
