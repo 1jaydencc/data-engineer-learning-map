5efba723678bc992d489b13d095e9df71c90eaef
1jaydencc <100813722+1jaydencc@users.noreply.github.com>
2023-09-05 17:12:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,62 @@
 import streamlit as st
+import plotly.graph_objects as go
+
+def create_roadmap():
+    milestones = ["Start", "Milestone A", "Milestone B", "Milestone C", "Finish"]
+
+    # Construct the figure
+    fig = go.Figure()
+
+    # Add a scatter plot for milestones
+    fig.add_trace(go.Scatter(
+        x=list(range(len(milestones))),
+        y=[1] * len(milestones),
+        mode="markers+text",
+        name="Milestones",
+        text=milestones,
+        textposition="bottom center"
+    ))
+
+    # Customize layout
+    fig.update_layout(
+        showlegend=False,
+        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+        plot_bgcolor='white'
+    )
+
+    return fig
 
 def display_learning_path():
-    st.title("Learning Path for Our Team")
+    st.title("Learning Path Roadmap")
 
-    progress = st.slider("Mark Your Progress:", 0, 100, 0)
-    st.progress(progress)
+    roadmap_fig = create_roadmap()
+    st.plotly_chart(roadmap_fig)
 
-    # Display path based on progress
-    if progress < 10:
-        st.write("1. Introduction to Topic A")
-    if progress < 20:
-        st.write("2. Workshop on Topic B")
-    # ... and so forth.
+    st.write("## Tasks & Courses per Milestone")
+    
+    # Example tasks & courses for each milestone
+    tasks_courses = {
+        "Milestone A": {
+            "tasks": ["Task A1", "Task A2"],
+            "courses": ["Course A1", "Course A2"]
+        },
+        "Milestone B": {
+            "tasks": ["Task B1"],
+            "courses": ["Course B1", "Course B2", "Course B3"]
+        },
+        # ... add more as needed
+    }
+
+    for milestone, data in tasks_courses.items():
+        st.write(f"### {milestone}")
+        st.write("Tasks:")
+        for task in data["tasks"]:
+            checkbox_key = f"task_{milestone}_{task}"
+            st.checkbox(task, key=checkbox_key)
+        st.write("Courses:")
+        for course in data["courses"]:
+            st.write(f"- {course}")
 
 if __name__ == "__main__":
     display_learning_path()
