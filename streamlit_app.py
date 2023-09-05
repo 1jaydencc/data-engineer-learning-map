8dbb7e48be619ba272c97ec7d835e2125caedd4c
1jaydencc <100813722+1jaydencc@users.noreply.github.com>
2023-09-05 18:22:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,16 +8,24 @@ def render_mermaid_chart(diagram_code):
     components.html(html_code, height=800)
 
 st.title("Learning Path")
+
+# Retrieve or initialize the session state for checkboxes
+session_state = SessionState.get(task_states={})
+
 learning_path = {
     "Beginner": ["Task 1", "Task 2", "Task 3"],
     "Intermediate": ["Task 4", "Task 5"],
     "Advanced": ["Task 6", "Task 7", "Task 8", "Task 9"],
 }
+
 diagram_code = "graph TD\n"
 prev_level = None
+
+# 1. First pass to generate the Mermaid code using stored checkbox states
 for level, tasks in learning_path.items():
     for i, task in enumerate(tasks):
-        checked = st.checkbox(task)
+        task_key = f"{level}_{task}"
+        checked = session_state.task_states.get(task_key, False)
         node_id = f"{level[0]}{i}"
         diagram_code += f'{node_id}["{task} {"(Done)" if checked else ""}"]\n'
         if prev_level:
@@ -26,5 +34,11 @@ for level, tasks in learning_path.items():
             diagram_code += f"{level[0]}{i-1} --> {node_id}\n"
     prev_level = f"{level[0]}{len(tasks)-1}"
 
-
+# Render the Mermaid chart
 render_mermaid_chart(diagram_code)
+
+# 3. Second pass to display the checkboxes
+for level, tasks in learning_path.items():
+    for task in tasks:
+        task_key = f"{level}_{task}"
+        session_state.task_states[task_key] = st.checkbox(task, value=session_state.task_states.get(task_key, False), key=task_key)
