391c2e037f4c82c3aa6c248835798df67ed2ebec
1jaydencc <100813722+1jaydencc@users.noreply.github.com>
2023-09-05 17:34:01 -0400

    Update SessionState.py

--- SessionState.py
+++ SessionState.py
@@ -1,14 +1,19 @@
 import streamlit as st
 
 class _SessionState:
+
     def __init__(self, **kwargs):
+        """A new SessionState object."""
         for key, val in kwargs.items():
             setattr(self, key, val)
 
 
 def get(**kwargs):
-    """Gets a SessionState object for the current session.
-    Creates a new object if necessary."""
-    if 'session_state' not in st.session_info:
-        st.session_info.session_state = _SessionState(**kwargs)
-    return st.session_info.session_state
+    """Gets a SessionState object for the current session. Creates one if necessary."""
+    
+    # Key we use to store a session state in the current session.
+    key = '_session_state'
+    
+    if not hasattr(st, key):
+        st._session_state = _SessionState(**kwargs)
+    return st._session_state
