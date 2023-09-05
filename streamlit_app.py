45e6d7b1a872166203b336d3773a841b96ff1cea
1jaydencc <100813722+1jaydencc@users.noreply.github.com>
2023-09-05 18:36:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,4 +29,6 @@ for level, tasks in learning_path.items():
         if i > 0:
             diagram_code += f"{level[0]}{i-1} --> {node_id}\n"
     prev_level = f"{level[0]}{len(tasks)-1}"
+
+print()
 render_mermaid_chart(diagram_code)
