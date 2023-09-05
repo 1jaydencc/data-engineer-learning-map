9ecf2662d6150b6aa26d75a6076555cdf11a078d
1jaydencc <100813722+1jaydencc@users.noreply.github.com>
2023-09-05 18:20:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,34 +8,23 @@ def render_mermaid_chart(diagram_code):
     components.html(html_code, height=800)
 
 st.title("Learning Path")
-
 learning_path = {
     "Beginner": ["Task 1", "Task 2", "Task 3"],
     "Intermediate": ["Task 4", "Task 5"],
     "Advanced": ["Task 6", "Task 7", "Task 8", "Task 9"],
 }
-
 diagram_code = "graph TD\n"
 prev_level = None
-
-task_states = SessionState.get(task_states={})
-
 for level, tasks in learning_path.items():
     for i, task in enumerate(tasks):
-        task_key = f"{level}_{task}"
-        checked = st.checkbox(task, value=task_states[task_key], key=task_key)
-        task_states[task_key] = checked
+        checked = st.checkbox(task)
         node_id = f"{level[0]}{i}"
-        
-        diagram_code += f"{node_id}[{task}{' (Done)' if checked else ''}]\n"
-        
+        diagram_code += f'{node_id}["{task} {"(Done)" if checked else ""}"]\n'
         if prev_level:
             diagram_code += f"{prev_level} --> {node_id}\n"
         if i > 0:
             diagram_code += f"{level[0]}{i-1} --> {node_id}\n"
     prev_level = f"{level[0]}{len(tasks)-1}"
 
-diagram_code += "\n"
 
 render_mermaid_chart(diagram_code)
-
