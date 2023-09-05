0e76cf47def8646e704a96d4a20498e00aeecb18
1jaydencc <100813722+1jaydencc@users.noreply.github.com>
2023-09-05 17:46:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,35 +6,27 @@ def render_mermaid_chart(diagram_code):
         html_code = f.read().replace("{{ diagram_code }}", diagram_code)
     components.html(html_code, height=800)
 
-# Data
-milestones = {
-    'Milestone 1': {
-        'Tasks': {
-            'Task 1': ['Course 1', 'Course 2'],
-            'Task 2': ['Course 3']
-        }
-    },
-    'Milestone 2': {
-        'Tasks': {
-            'Task 3': ['Course 4', 'Course 5'],
-            'Task 4': []
-        }
-    }
+st.title("Learning Path")
+
+learning_path = {
+    "Beginner": ["Task 1", "Task 2", "Task 3"],
+    "Intermediate": ["Task 4", "Task 5"],
+    "Advanced": ["Task 6", "Task 7", "Task 8", "Task 9"],
 }
 
-# Create a session state for user's progress
-session_state = SessionState.get(progress={})
+diagram_code = "graph TD\n"
+prev_level = None
 
-# Display data
-for milestone, details in milestones.items():
-    st.markdown(f"### ➡️ {milestone}")
-    
-    tasks = details['Tasks']
-    for task, courses in tasks.items():
-        task_status = session_state.progress.get(task, False)
-        session_state.progress[task] = st.checkbox(task, value=task_status)
-        
-        # Display linked courses
-        for course in courses:
-            course_status = session_state.progress.get(course, False)
-            session_state.progress[course] = st.checkbox(course, value=course_status, key=course)
+for level, tasks in learning_path.items():
+    for i, task in enumerate(tasks):
+        checked = st.checkbox(task)
+        node_id = f"{level[0]}{i}"
+        diagram_code += f"{node_id}[{task} {'(Done)' if checked else ''}]\n"
+        if prev_level:
+            diagram_code += f"{prev_level} --> {node_id}\n"
+        if i > 0:
+            diagram_code += f"{level[0]}{i-1} --> {node_id}\n"
+    prev_level = f"{level[0]}{len(tasks)-1}"
+
+render_mermaid_chart(diagram_code)
+
