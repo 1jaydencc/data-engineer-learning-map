a8cc9718912da3afe8e17e71fcbdde986f638fd1
1jaydencc <100813722+1jaydencc@users.noreply.github.com>
2023-09-05 22:26:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,12 @@ def render_mermaid_chart(diagram_code):
     components.html(html_code, height=800)
 
 st.title("Data Engineering Learning Map")
-
 # Starting point of the diagram_code
 diagram_code = "graph TD\n"
 
 # Hardcoded diagram code for "Getting Started"
 task1_id = "B0"
+sub_items_task1 = ["Architecture overview", "Data engineering life cycle", "Data storage capabilities"]
 diagram_code += f'{task1_id}["Getting Started"]\n'
 
 sub_node_prefix = f"{task1_id}S"
